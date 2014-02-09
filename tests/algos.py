d5e9535dda8a9cd0f301378e35e8158721398d31
gratefulfrog <gratefulfrog@gmail.com>
2014-02-09 19:29:47 +0100

    futher algo work, there is a defect somewhere incoherent use of ccs

--- tests/algos.py
+++ tests/algos.py
@@ -7,18 +7,29 @@ Some algorithm work for vision and so on
 from math import *
 
 def runTests():
-    b = 20  # pixies are separated by 20cm
-    Targets= [(1600,2600,-500),  # targets are +/-16m off x-axis
-              (-1600,2600,-500), # 26m away on y-axis
-              (1600,2600,500),   # +/-5m off z-axis
-              (-1600,2600,500)]
+    """
+    The test cases:
+    if the observer is at (0,0,0):
+    * if the targetY <= ObserverY, FAIL
+    * if the targetX or targetZ are outside the Observer field of view, FAIL
+    * computed dy = O(y) - T(y), if not FAIL
+    * computed dx = O(x) - T(x), if not FAIL
+    * computed dz = 0(z) - T(z), if not FAIL
+    Test Values:
+    * Obs = (0,0,0)
+    """
+    b = .20  # pixies are separated by 20cm
+    Targets= [(-16,-26,-5),  # targets are +/-16m off x-axis
+              (+16,-26,-5), # 26m away on y-axis
+              (-16,-26,5),   # +/-5m off z-axis
+              (+16,-26,5)]
 
     testNumber = 0
     epsilon = 0.001  # for equality
     for Target in Targets:
-        al=atan2(Target[1],Target[0]+b/2.0)
-        ar=atan2(Target[1],Target[0]-b/2.0)
-        ah=atan2(Target[2],Target[1])
+        al=atan2(-Target[1],-Target[0]+b/2.0)
+        ar=atan2(-Target[1],-Target[0]-b/2.0)
+        ah=atan2(-Target[2],-Target[1])
         res= dxyzObserverTarget(b,al,ar,ah)
         print [round(r,2) for r in res]
         if all( abs(x-y)<epsilon for x,y in \
@@ -54,6 +65,10 @@ def d(b,al,ar):
 def a(b,al,ar,dist=None):
     if not dist:
         dist = d(b,al,ar)
+    print degrees(al),degrees(ar)
+    print dist
+    print R(b,al,ar)
+    print sin(pi-ar)
     res = asin(R(b,al,ar)*sin(pi-ar)/dist)
     if ar>al>pi/2.0:
         res = pi -res
@@ -61,7 +76,7 @@ def a(b,al,ar,dist=None):
 
 def dxyzObserverTarget(b,al,ar,ah):
     """ calculed such that the result is positve if the target is at 
-    an x or y or z value greater than 0
+    an x or y or z value less than the observer's values
     dz is calculated such that if the angle ah is positive then the distance
     in Z is positive,  ah is positive when observer is below target, ah is zero
     when observer is level with target, ah is negative when observer is above
@@ -70,8 +85,13 @@ def dxyzObserverTarget(b,al,ar,ah):
     """
     doAssert(al,ar,ah)
     dist = d(b,al,ar)
-    dy = dist*sin(a(b,al,ar,dist))
-    return (dist*cos(a(b,al,ar,dist)), dy, dy*tan(ah))
+    dy = -dist*sin(a(b,al,ar,dist))
+    dx = abs(dist*cos(a(b,al,ar,dist)))
+    if (al < pi-ar):
+        dx=-dx
+    dz = dy*tan(ah)
+    
+    return (dx, dy, dz)
 
 
 PixieHorizontalField = radians(75.0)
@@ -90,6 +110,10 @@ class Pixie:
         self.PDx = (self.centerX)/tan(self.fx/2.0)
         self.PDy = (self.centerY)/tan(self.fy/2.0)
         self.Xoffset = offset
+
+    def pixel2Anglep(self,(px,py)):
+        return self.pixel2Angle(px,py)
+
 
     def pixel2Angle(self,px,py):
         """ return a pair (a,ah)
@@ -132,13 +156,34 @@ class Pixie:
 
     # now we need a function that takes the oberserver(x,y,z)
     # and returns the pixel values for each pixie Left and Right (px,py)
-    def targetObserverXYZ2PixiePxPy(self,target,observer):
+    def observerTargetXYZ2PixiePxPy(self,target,observer):
         """ return the pixel pair (px,py)
         """
-        dy = target[1]-observer[1]
-        px = ((target[0]-(observer[0] + self.Xoffset))*self.PDx/dy) + \
+        dy = observer[1]- target[1]
+        px = (((observer[0] + self.Xoffset-target[0]))*self.PDx/dy) + \
             self.centerX
-        py  = (((target[2]-observer[2])*self.PDy)/dy) + self.centerY
+        py  = (((observer[2]-target[2])*self.PDy)/dy) + self.centerY
         return (int(round(px,0)),int(round(py,0)))
 
-# still need to test, but seems good to go!!
+# still need to test, DOES NOT seem good to go!!
+# still an issue for the signs of dx, dy dz
+
+class SimDrone:
+    def __init__(self,b = 0.2):
+        self.B=b
+        self.pixieL = Pixie(-self.B/2.0)
+        self.pixieR = Pixie(self.B/2.0)
+        self.position = (0.0,0.0,0.0)
+
+
+## still needs work...
+    def dxyz(self,targetXYZ):
+        (al,ahL) = self.pixieL.pixel2Anglep(
+            self.pixieL.observerTargetXYZ2PixiePxPy(targetXYZ,
+                                                    self.position))
+        (ar,ahR) = self.pixieR.pixel2Anglep(
+            self.pixieR.observerTargetXYZ2PixiePxPy(targetXYZ,
+                                                    self.position))
+        print "al:%f ar:%f ahL:%f ahR:%f"%(al,ar,ahL, ahR)
+        return dxyzObserverTarget(self.B,((pi/2.0) -al),((pi/2.0)-ar),(ahL+ahR)/2.0)
+    
